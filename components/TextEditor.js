c3f99db564500ce0941d09a425afa07ce1b60282
Jamey Smith <smith.jameyd@gmail.com>
2021-08-19 14:50:52 -0700

    Finished build

---
+++ components/TextEditor.js
@@ -0,0 +1,61 @@
+import dynamic from "next/dynamic";
+import { useState, useEffect } from "react";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { EditorState } from "draft-js";
+import { db } from "../firebase";
+import { useRouter } from "next/dist/client/router";
+import { convertToRaw, convertFromRaw } from "draft-js";
+import { useSession } from "next-auth/client";
+import { useDocumentOnce } from "react-firebase-hooks/firestore";
+
+const Editor = dynamic(
+  () => import("react-draft-wysiwyg").then((module) => module.Editor),
+  { ssr: false }
+);
+
+function TextEditor() {
+  const [session] = useSession();
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [snapshot] = useDocumentOnce(
+    db.collection("userDocs").doc(session.user.email).collection("docs").doc(id)
+  );
+
+  useEffect(() => {
+    if (snapshot?.data()?.editorState) {
+      setEditorState(
+        EditorState.createWithContent(
+          convertFromRaw(snapshot?.data()?.editorState)
+        )
+      );
+    }
+  }, [snapshot]);
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+
+    db.collection("userDocs")
+      .doc(session.user.email)
+      .collection("docs")
+      .doc(id)
+      .set(
+        { editorState: convertToRaw(editorState.getCurrentContent()) },
+        { merge: true }
+      );
+  };
+
+  return (
+    <div className="bg-[#f8f9fa] min-h-screen pb-16">
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={onEditorStateChange}
+        toolbarClassName="flex sticky top-0 z-50 !justify-center mx-auto"
+        editorClassName="mt-6 bg-white shadow-lg max-w-4xl mx-auto mb-12 border p-10"
+      />
+    </div>
+  );
+}
+
+export default TextEditor;
